0987248e0472b11aa0cde4e47b248c20f95f892a
Ryan Gibb <ryangibb321@gmail.com>
2021-07-06 19:06:13 +0100

    updates

--- dotfiles/.config/sway/scripts/bat_monitor.sh
+++ dotfiles/.config/sway/scripts/bat_monitor.sh
@@ -5,16 +5,11 @@ do
 	acpi -b | awk -F'[,:%]' '{print $2, $3}' | {
 		read -r status capacity
 	
-		if [ "$status" = Discharging -a "$capacity" -lt 15 ]; then
-			notify-send "warning: battery at $capacity%"	
-		fi
 		if [ "$status" = Discharging -a "$capacity" -lt 5 ]; then
 			logger "Critical battery threshold"
-			systemctl suspend-then-hibernate
-		fi
-		if [ "$status" = Discharging -a "$capacity" -lt 3 ]; then
-			logger "Critical battery threshold"
 			systemctl hibernate
+		elif [ "$status" = Discharging -a "$capacity" -lt 15 ]; then
+			notify-send "warning: battery at $capacity%"	
 		fi
 	}
 	sleep 60
