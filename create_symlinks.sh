d2e5bea4c0a8e83ad9179b1be024db4aa754e8b8
Ryan Gibb <ryangibb321@gmail.com>
2021-07-20 23:30:30 +0100

    symlink updates

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -4,13 +4,15 @@ cd "$(dirname $0)/dotfiles"
 PROJECT_DIR="$(pwd)"
 
 LINKS=(
-	.config/*
+	$(find .config -maxdepth 2 -type f -printf '.config/%P\n')
+	$(find .config -maxdepth 2 -mindepth 2 -type d -printf '.config/%P\n')
 	$(find . -maxdepth 1 -type f -printf '%P\n')
 	.local/share/applications/*.desktop
 )
 
 for link in "${LINKS[@]}"; do
 	echo "$HOME/$(dirname "$link") -> $PROJECT_DIR/$link"
+	mkdir -p "$HOME/$(dirname "$link")"
 	ln -sf "$PROJECT_DIR/$link" "$HOME/$(dirname "$link")"
 done
 
