8f15c3dd2ac0e337a1fe2ff255cf67108567bfe7
Ryan Gibb <ryangibb321@gmail.com>
2022-01-19 10:47:00 +0000

    swayidle physlock

--- home/.config/sway/scripts/lock_on_lid_close.sh
+++ home/.config/sway/scripts/lock_on_lid_close.sh
@@ -2,6 +2,6 @@
 
 # lock if only monitor
 if [ "$(swaymsg -t get_outputs | grep '"type": "output"' | wc -l)" = "1" ]; then
-	swaylock -f -i ~/pictures/wallpapers/default
+	loginctl lock-session
 fi
 
